41ed723d399a9b7550df679f3cd79fe8663cd428
kevinGaniswarna <kevin.ganiswarna@hotmail.com>
2023-08-08 22:45:13 +0700

    stockScreener Prereq

--- introductory_finance.py
+++ introductory_finance.py
@@ -25,7 +25,7 @@ import psycopg2, psycopg2.extras
 from pandas_datareader import data as pdr
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
+from pandas import excelWriter
 
 
 
@@ -361,10 +361,115 @@ if option == "backtest":
 
 if option == "stock screener":
     st.subheader("this is the stock screener dashboard")
+    yf.pdr_override()
 
+    startyear = 2020
+    startmonth = 1
+    startdate = 1
+
+    start = dt.datetime(startyear,startmonth, startdate)
+
+    now = dt.datetime.now()
+
+    filePath = r"C:\Users\Kevin Ganis\Documents\kevin personal doc\financial_Dashboard\RichardStocks.xlsx"
+    stocklist = pd.read_excel(filePath)
+    stocklist = stocklist.head()
+    exportList = pd.DataFrame(columns = ["Stock","RS_Rating", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low", "52 week High"])
+    exportList.append("Stock": stock, "RS_rating": RS_rating, "50 Day MA": ma_50,)
+
+    for i in stocklist.index :
+        stock = str(stocklist["Symbol"][i])
+        RS_rating = stocklist["RS Rating"][i]
+
+        try:
+            df = pdr.get_data_yahoo(stock, start, now)
+
+            smaUsed = [50,150, 200]
+            for x in smasUsed:
+                sma = x
+                smaString = "SMA_" + str(sma)
+                df[smaString] = round(df.iloc[:,4].rolling(window = sma).mean(),2)
+
+            currentClose = df["Adj Close"][-1]
+            moving_average_50 = df["SMA_50"][-1]
+            moving_average_150 = df["SMA_150"][-1]
+            moving_average_200 = df["SMA_200"][-1]
+            low_of_52_week = min(df["Adj Close"][-260:])
+            high_of_52_week = max(df["Adj Close"][-260:])
+
+            try:
+                moving_average_200_20past = df["SMA_200"][-20]
+            except Exception:
+                moving_average_200_20past = 0
+
+            st.write("Checking"+ stock + "......")
+
+
+            # condition 1 current price ? 150 SMA and > 200 SMA
+            if(currentClose > moving_average_150 and currentClose > moving_average_200):
+                cond_1 = True
+            else:
+                cond_1 = False
+
+            # condition 2 150 SMA >200 SMA
+            if( moving_average_150 > moving_average_200):
+                cond_2 = True
+            else:
+                cond_2 = False
+
+
+            # Condition 3 200 SMA trending up for at least 1 month (ideally 4-5 months)
+            if( moving_average_200 > moving_average_200_20past):
+                cond_3 = True
+            else:
+                cond_3 = False
+
+            # Condition 4 50 SMA > 150 SMA and 50 SMA > 200 SMA
+            if(moving_average_50 > moving_average_150 and moving_average_50 > moving_average_200):
+                cond_4 = True
+            else:
+                cond_4 = False
+            # condition 5 Current Price > 50 SMA
+            if(currentClose > moving_average_50):
+                cond_5 = True
+            else:
+                cond_5 = False
+
+            # condition 6 Current price at least 30% above 52 week low ( Many of teh best are up 100 - )
+            if(currentClose >= (1.3*low_of_52_week) ):
+                cond_6 = True
+            else:
+                cond_6 = False
+            # Condition 7 Current price is within 25% of 52 week high
+            if(currentClose >= 0.75*high_of_52_week):
+                cond_7 = True
+            else:
+                cond_7 = False
+            # Condition 8 IBD RS rating > 70 and higher the better
+            if(RS_rating > 70):
+                cond_8 = True
+            else:
+                cond_8 = False
+
+            if(cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8):
+                row = pd.Series([stock, RS_rating, moving_average_50, moving_average_150, moving_average_200, low_of_52week, high_of_52week], index = ["Stock", "RS_rating", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low",  "52 week High"])
+			    exportList = exportList.append(row, ignore_index=True)
+
+
+        except Exception:
+            st.write("No data on " + stock)
+
+    st.write(exportList)
+    # write output exportlist into excel
+    newFile = os.path(filePath) + "/ScreenOutput.xlsx"
+
+    writer = ExcelWriter(newFile)
+    exportList.to_excel(writer, "Sheet1")
+    writer.save()
 
 if option == "twitter":
     st.subheader("this is the twitter dashboard")
+
     # for username in config.TWITTER_USERNAMES:
     #     user = api.get_user(username)
     #     tweets = api.user_timeline(username)
