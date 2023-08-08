e8858069d90ca7e327f6d3ed66a606597c1a3626
kevinGaniswarna <kevin.ganiswarna@hotmail.com>
2023-08-08 23:31:39 +0700

    need to debug stock screener. Passed stocks are not added in the exportList array. Need further investigations

--- introductory_finance.py
+++ introductory_finance.py
@@ -25,7 +25,7 @@ import psycopg2, psycopg2.extras
 from pandas_datareader import data as pdr
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-from pandas import excelWriter
+
 
 
 
@@ -375,20 +375,22 @@ if option == "stock screener":
     stocklist = pd.read_excel(filePath)
     stocklist = stocklist.head()
     exportList = pd.DataFrame(columns = ["Stock","RS_Rating", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low", "52 week High"])
-    exportList.append("Stock": stock, "RS_rating": RS_rating, "50 Day MA": ma_50,)
+    st.write("initial export list are as follows:")
+    st.write(exportList)
 
     for i in stocklist.index :
         stock = str(stocklist["Symbol"][i])
+        st.write(stock)
         RS_rating = stocklist["RS Rating"][i]
-
         try:
             df = pdr.get_data_yahoo(stock, start, now)
-
+            st.write(df)
             smaUsed = [50,150, 200]
-            for x in smasUsed:
-                sma = x
-                smaString = "SMA_" + str(sma)
-                df[smaString] = round(df.iloc[:,4].rolling(window = sma).mean(),2)
+
+            for x in smaUsed:
+                sma=x
+                df["SMA_"+str(sma)]=round(df.iloc[:,4].rolling(window=sma).mean(),2)
+
 
             currentClose = df["Adj Close"][-1]
             moving_average_50 = df["SMA_50"][-1]
@@ -401,9 +403,6 @@ if option == "stock screener":
                 moving_average_200_20past = df["SMA_200"][-20]
             except Exception:
                 moving_average_200_20past = 0
-
-            st.write("Checking"+ stock + "......")
-
 
             # condition 1 current price ? 150 SMA and > 200 SMA
             if(currentClose > moving_average_150 and currentClose > moving_average_200):
@@ -418,7 +417,7 @@ if option == "stock screener":
                 cond_2 = False
 
 
-            # Condition 3 200 SMA trending up for at least 1 month (ideally 4-5 months)
+                # Condition 3 200 SMA trending up for at least 1 month (ideally 4-5 months)
             if( moving_average_200 > moving_average_200_20past):
                 cond_3 = True
             else:
@@ -435,7 +434,7 @@ if option == "stock screener":
             else:
                 cond_5 = False
 
-            # condition 6 Current price at least 30% above 52 week low ( Many of teh best are up 100 - )
+                # condition 6 Current price at least 30% above 52 week low ( Many of teh best are up 100 - )
             if(currentClose >= (1.3*low_of_52_week) ):
                 cond_6 = True
             else:
@@ -445,27 +444,35 @@ if option == "stock screener":
                 cond_7 = True
             else:
                 cond_7 = False
-            # Condition 8 IBD RS rating > 70 and higher the better
+                # Condition 8 IBD RS rating > 70 and higher the better
             if(RS_rating > 70):
                 cond_8 = True
             else:
                 cond_8 = False
 
-            if(cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8):
-                row = pd.Series([stock, RS_rating, moving_average_50, moving_average_150, moving_average_200, low_of_52week, high_of_52week], index = ["Stock", "RS_rating", "50 Day MA", "150 Day MA", "200 Day MA", "52 Week Low",  "52 week High"])
-			    exportList = exportList.append(row, ignore_index=True)
 
+            ## DEBUG AROUND HERE REQUIRED
+            st.write([cond_1, cond_2, cond_3, cond_4, cond_5, cond_6, cond_7, cond_8])
+            st.write(cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8)
+            row =  {'Stock': stock, "RS_Rating": RS_Rating, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}
+            st.write(row)
+
+            if (cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6 and cond_7 and cond_8):
+                row = {'Stock': stock, "RS_Rating": RS_Rating, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}
+                exportList = exportList.append(row, ignore_index=True)
+            else:
+                exportList = exportList
 
         except Exception:
             st.write("No data on " + stock)
 
+    st.write("final export list are as follows: ")
     st.write(exportList)
     # write output exportlist into excel
-    newFile = os.path(filePath) + "/ScreenOutput.xlsx"
-
-    writer = ExcelWriter(newFile)
-    exportList.to_excel(writer, "Sheet1")
-    writer.save()
+    newFile=os.path.dirname(filePath)+"\ScreenOutput.xlsx"
+    writer= pd.ExcelWriter(newFile)
+    exportList.to_excel(writer,"Sheet1")
+    #writer.save()
 
 if option == "twitter":
     st.subheader("this is the twitter dashboard")
